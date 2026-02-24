a9f40bbba967ecfeed7451cc60eb6029269a6eaa
Sandeep Kumar <sandeepkumarbarik9501@gmail.com>
2026-02-24 21:13:34 +0530

    Added Check Analytics Part

--- controllers/url.js
+++ controllers/url.js
@@ -1,5 +1,6 @@
 const shortid = require('shortid')
-const URL = require('../models/url')
+const URL = require('../models/url');
+const { response } = require('express');
 
 async function handleGenerateNewShortURL(req, res){
     const body = req.body;
@@ -17,6 +18,15 @@ async function handleGenerateNewShortURL(req, res){
     return res.json({id: shortID})
 }
 
+async function handleGetAnalytics(req, res){
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({ shortId })
+    return res.json({ totalClicks: result.visitHistory.length,
+        analytics: result.visitHistory,
+     })
+}
+
 module.exports = {
     handleGenerateNewShortURL,
+    handleGetAnalytics,
 }
